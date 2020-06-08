676f5728736c936d21af99b4ba6c06172069f964
Jack Quinoa <jquinoa208@gmail.com>
2020-06-08 22:40:19 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -43,7 +43,7 @@ class Owner
   end
   
   def walk_dogs
-    Dog.each {|dog| dog.owner == self ? dog.mood = "happy" end}
+    Dog.each {|dog| dog.owner == self ? dog.mood = "happy"} end
   end
   
   def feed_cats
