c8c91b1d553817fbf8d8a080d325c116163f1ae9
Jack Quinoa <jquinoa208@gmail.com>
2020-06-08 22:13:02 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -28,8 +28,7 @@ class Owner
   
   def cats 
   #  binding.pry
-    owned_cats = Cat.owner.all.match(self)   
-    owned_cats
+    Cat.all.select {|cat| cat.owner == self } 
   end
   
   def dogs 
